58181e601a7d6b6d8a689cde14556808ed378d5d
Saransh <17107201s@gamil.com>
2024-02-08 19:39:59 +0530

    made some changes

--- components/project.tsx
+++ components/project.tsx
@@ -19,11 +19,11 @@ export default function Project({ title, description, tags, imageUrl, projectUrl
             <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
                 <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
                     <h3 className="text-2xl font-semibold">{title}</h3>
-                    <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
-                    <Link className='group bg-gray-900 text-white text-[1rem] px-3 py-1 flex flex-wrap rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition' href={projectUrl}>Project Link</Link>
+                    <p className="mt-2 leading-relaxed text-[0.9rem] text-gray-700 dark:text-white/70">{description}</p>
+                    <Link className='group bg-gray-900 text-white text-[0.75rem] px-3 py-1 flex flex-wrap rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition p-3' href={projectUrl}>Project Link</Link>
                     <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
                         {tags.map((tag, index) => (
-                            <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70" key={index}>
+                            <li className="bg-black/[0.7] px-3 py-1 text-[0.6rem] uppercase tracking-wider text-white rounded-full dark:text-white/70" key={index}>
                                 {tag}
                             </li>
                         ))}
--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,8 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import DarthVader from "@/public/Darth_Vader.jpg";
+import Ecommerce from "@/public/Ecommerce.jpg";
+import Opensoft from "@/public/Opensoft_2022.jpg";
+import ProductDesign from "@/public/Product_Design_2023.jpg";
 
 export const links = [
     {
@@ -32,11 +34,25 @@ export const links = [
 export const projectsData = [
     {
         title: "Ecommerce",
-        description: "My Ecommerce project has features like filtering, sorting, pagination, authentication and payment. Please have a look.",
+        description: "My Ecommerce project has features like filtering, sorting, pagination, authentication and payment. It has an admin dashboard for analytics and management.",
         tags: ["React", "Node", "MongoDB", "Express", "Redux", "TypeScript"],
         projectUrl: "https://ecommerce-frontend-r19h6i23s-saranshs-projects.vercel.app/",
-        imageUrl: DarthVader
+        imageUrl: Ecommerce
     },
+    {
+        title: "Opensoft GC 2022, IIT Kharagpur",
+        description: "Create a food delivery application to ensure the availability of essential services during the downtime of data centre. Top 5",
+        tags: ["Django", "React", "Grafana", "Kubernetes", "Docker"],
+        projectUrl: "https://github.com/rkhall-iitkgp/restaurant-app-frontend",
+        imageUrl: Opensoft
+    },
+    {
+        title: "Product Design GC 2023, IIT Kharagpur",
+        description: "Design a hardware product to ensure that the rider wears the helmet all the time and does not ride under the influence of alcohol via restricting the use in these circumstances. Gold",
+        tags: [],
+        projectUrl: "https://drive.google.com/file/d/1nr8PAh3EPaDKv98-92FEQfS-VxSuWMMx/view?usp=sharing",
+        imageUrl: ProductDesign
+    }
 ] as const;
 
 export const experiencesData = [
