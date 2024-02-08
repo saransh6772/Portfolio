6328385724f998a9cc06adcdbe83950cc9dc8e43
Saransh <17107201s@gamil.com>
2024-02-08 16:22:17 +0530

    made some changes

--- components/about.tsx
+++ components/about.tsx
@@ -11,9 +11,9 @@ export default function About() {
         <motion.section ref={ref} className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.175 }} id="about">
             <SectionHeading>About me</SectionHeading>
             <p className="mb-3">
-                I am a final year undergraduate student at the <span className="font-medium">Indian Institute of Technology, Kharagpur</span> with major in <span className="font-medium">Biotechnology and Biochemical Engineering</span>.
+                I am a final year undergraduate student at the <span className="font-medium">Indian Institute of Technology, Kharagpur</span>, majoring in <span className="font-medium">Biotechnology and Biochemical Engineering</span>.
                 <br />With a strong passion for problem-solving and technology, I aspire to apply my detail-oriented mindset and technical skills throughout my career.
-                <br />I love programming, and I have a keen interest in <span className="font-medium">Data Structures and Algorithms</span>.
+                <br />I love programming, and am keenly interest in <span className="font-medium">Data Structures and Algorithms</span>.
                 <br />My core stack is <span className="font-medium">React, Next.js, Node.js, and MongoDB</span> and familiar with TypeScript & Prisma.
                 <br /> I am currently looking for a <span className="font-medium">full-time position</span> as a software developer.
             </p>
