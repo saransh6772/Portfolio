64e0d950a48ff36a6e49c71b28d431f87315f408
Saransh <17107201s@gamil.com>
2024-05-15 16:10:41 +0530

    cv update

--- components/about.tsx
+++ components/about.tsx
@@ -15,7 +15,7 @@ export default function About() {
                 <br />With a strong passion for problem-solving and technology, I aspire to apply my detail-oriented mindset and technical skills throughout my career.
                 <br />I love programming, and am keenly interest in <span className="font-medium">Data Structures and Algorithms</span>.
                 <br />My core stack is <span className="font-medium">React, Next.js, Node.js, and MongoDB</span> and familiar with TypeScript & Prisma.
-                <br /> I am currently looking for a <span className="font-medium">full-time position</span> as a software developer.
+                <br />I am currently looking for a <span className="font-medium">full-time position</span> as a software developer.
             </p>
             <p>
                 <span className="italic">When I'm not coding</span>, I enjoy watching <span className="font-medium">Anime, Movies and Series</span>.
